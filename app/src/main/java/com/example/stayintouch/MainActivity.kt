229c0300b5ecbf6a64d2fb4f40ef9e53643512c1
Mahima <mahima.6.ms@gmail.com>
2019-06-18 22:28:57 +0530

    Initial commit

---
+++ app/src/main/java/com/example/stayintouch/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.stayintouch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
